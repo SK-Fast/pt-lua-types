3857b726f11aa740fce3fda2493bb86b14919fa3
devy <60780448+SK-Fast@users.noreply.github.com>
2023-10-20 02:57:50 +0700

    Installer

--- installer/index.js
+++ installer/index.js
@@ -1,8 +1,8 @@
 const cp = require('child_process')
 const fs = require('fs')
-const Downloader = require("nodejs-file-downloader");
-const sevenZ = require("node-7z");
-const axios = require('axios').default
+const path = require('path')
+const roamingPath = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Preferences' : process.env.HOME + "/.local/share")
+const configPath = path.resolve(roamingPath, "Code/User/settings.json")
 
 async function main() {
     console.log("Checking extension...")
@@ -21,10 +21,27 @@ async function main() {
 
         cp.execSync("code --install-extension sumneko.lua")
     }
+    
+    if (fs.existsSync("./pt-lua-types")) {
+        console.log("Removing old installation...")
+        fs.rmSync("pt-lua-types", {recursive: true})
+    }
 
     console.log("Downloading Repo via git...")
     cp.execSync('git clone https://github.com/SK-Fast/pt-lua-types.git')
     
+    console.log("Adding Polytoria to Configuration")
+    let configOrigin = JSON.parse(fs.readFileSync(path.resolve(roamingPath, "Code/User/settings.json")))
+    configOrigin["Lua.workspace.library"] = [ path.resolve(__dirname, "pt-lua-types") ]
+    fs.writeFileSync(configPath, JSON.stringify(configOrigin))
+
+    console.log("Completed")
+
+    console.log('Press any key to exit');
+
+    process.stdin.setRawMode(true);
+    process.stdin.resume();
+    process.stdin.on('data', process.exit.bind(process, 0));
 }
 
 main()
