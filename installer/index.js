303463be12d0c47dcf2f16b4b6444674eb33a2fd
devy <60780448+SK-Fast@users.noreply.github.com>
2023-10-20 02:47:30 +0700

    rearrange files

--- installer/index.js
+++ installer/index.js
@@ -1,31 +1,33 @@
 const cp = require('child_process')
 const fs = require('fs')
-const download = require('download-file')
+const Downloader = require("nodejs-file-downloader");
+const sevenZ = require("node-7z");
 const axios = require('axios').default
-console.log("Checking extension...")
-const execResult = cp.execSync("code --list-extensions").toString()
 
-let installed = false
-for (const extName of execResult.split("\n")) {
-    if (extName == "sumneko.lua") {
-        installed = true
-        break
+async function main() {
+    console.log("Checking extension...")
+    const execResult = cp.execSync("code --list-extensions").toString()
+
+    let installed = false
+    for (const extName of execResult.split("\n")) {
+        if (extName == "sumneko.lua") {
+            installed = true
+            break
+        }
     }
-}
 
-if (!installed) {
-    console.log("Installing sumneko.lua...")
+    if (!installed) {
+        console.log("Installing sumneko.lua...")
 
-    cp.execSync("code --install-extension sumneko.lua")
-}
+        cp.execSync("code --install-extension sumneko.lua")
+    }
 
-console.log("Downloading latest release...")
+    console.log("Downloading Repo via git...")
+    cp.execSync('git clone https://github.com/SK-Fast/pt-lua-types.git')
     
+}
 
-console.log("Modifying Config file...")
-
-
-console.log("Completed!")
+main()
 /*
 "Lua.workspace.library": [
         "P:\\projects\\lua\\pt-lua-types\\"
